14610fca7d304b6096efcafa38c1bc4bf31b37db
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2023-12-26 17:19:48 +0700

    - Set up vr_level_select with full customizability + Added vr_level_transition that handles level transitions in-between `SceneBase`s + Added `level_select`s for VR Main Menu and Quit Application - Modified stairs to have ramps as collisions - Added pointer_function to VR player to choose levels

--- assets/common/scenes/level_select/level_select.gd
+++ assets/common/scenes/level_select/level_select.gd
@@ -1,6 +1,7 @@
 extends Button
 class_name LevelSelect
 
+@export var is_quit := false
 @export_file("*.tscn") var level_path = ""
 @export var preview_image : Texture2D
 @export_multiline var level_description := "This is a type of description that a level might have. It should not be too long nor too short. It should contain all the things the visitors should know before entering the level."
--- assets/vr/scenes/main_menu/level_select/vr_level_select.gd
+++
@@ -1,3 +0,0 @@
-extends Node3D
-class_name VRLevelSelect
-
