f00e00dbf05efd0e41313b19c606a45856d5f240
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2023-12-27 10:47:27 +0700

    - Replaced assets/common by submodule

--- assets/common/scenes/sandbox/sandbox.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var world_environment: WorldEnvironment = $WorldEnvironment
-@onready var sky_rotation := world_environment.environment.sky_rotation.y
-
-func _process(delta: float) -> void:
-	sky_rotation += 0.006 * delta
-	world_environment.environment.sky_rotation.y = sky_rotation
