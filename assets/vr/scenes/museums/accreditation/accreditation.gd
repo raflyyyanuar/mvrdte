39c450a27c146482a3be242fbc40cbba0b13e017
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-01-18 20:31:36 +0700

    - Optimized accreditation.gd to support many accreditations

---
+++ assets/vr/scenes/museums/accreditation/accreditation.gd
@@ -0,0 +1,65 @@
+@tool
+extends Resource
+class_name Accreditation
+
+@export var year : int = 2004
+enum MajorEnum {
+	Empty, # 0
+	Teknik_Elektro_S1, # 1
+	Teknik_Elektro_S2, # 2
+	Teknik_Elektro_S3, # 3
+	Teknik_Komputer_S1, # 4
+	Teknik_Biomedik_S1, # 5
+	Teknik_Biomedik_S2, # 6
+}
+
+enum Grade {
+	A,
+	B,
+	Unggul,
+	Baik,
+	IAABE,
+}
+
+@export var majors : Array[MajorEnum]
+@export var grades : Array[Grade]
+
+
+func get_major_index(index : int) -> int:
+	if index > 4: return 2 # Biomedik
+	elif index == 4: return 1 # Komputer
+	elif index > 0: return 0 # Elektro
+	else: return -1 # Empty
+
+
+func get_grade_name(index: int) -> String:
+	match index:
+		0:
+			return "A"
+		1:
+			return "B"
+		2:
+			return "Unggul"
+		3:
+			return "Baik"
+		_:
+			return "-"
+
+
+func get_major_name(index : int) -> String:
+	match index:
+		1:
+			return "Teknik Elektro S1"
+		2:
+			return "Teknik Elektro S2"
+		3:
+			return "Teknik Elektro S3"
+		4:
+			return "Teknik Komputer S1"
+		5:
+			return "Teknik Biomedik S1"
+		6:
+			return "Teknik Biomedik S2"
+		_:
+			return ""
+
---
+++ assets/vr/scenes/museums/accreditation/accreditation_card.gd
@@ -0,0 +1,76 @@
+@tool
+extends PanelContainer
+class_name AccredicationCard
+
+@export var refresh := false : set = set_refresh
+@export var accreditation : Accreditation = null
+
+@onready var year = $HBoxContainer/VBoxContainer/Year
+@onready var majors = $HBoxContainer/VBoxContainer/MajorContainer.get_children()
+@onready var buttons = $HBoxContainer/ButtonContainer.get_children()
+
+@onready var logos = [
+	preload("res://assets/common/images/atlases/LogoElektro.png"),
+	preload("res://assets/common/images/atlases/LogoKomputer.png"),
+	preload("res://assets/common/images/atlases/LogoBiomed.png"),
+]
+
+var colors : Array[Color] = [
+	Color.AQUAMARINE,
+	Color.FUCHSIA,
+	Color.GOLD,
+	]
+
+
+func _ready():
+	set_refresh()
+
+
+func set_accreditation() -> void:
+	year.set_text(str(accreditation.year))
+	
+	for i in range(3):
+		majors[i].hide()
+		buttons[i].hide()
+	
+	for i in range(3):
+		if i >= accreditation.majors.size():
+			break
+		
+		# Empty? Elektro S1? Komputer S1? Biomedik S2?
+		var current_major = accreditation.majors[i]
+		
+		## -1 = empty
+		## 0 = elektro
+		## 1 = komputer
+		## 2 = biomedik
+		var major_index = accreditation.get_major_index(current_major)
+		if major_index == -1:
+			majors[i].set_visible(false)
+			buttons[i].set_visible(false)
+			continue
+		
+		var current_grade : int = 0
+		if i < accreditation.grades.size():
+			current_grade = accreditation.grades[i]
+		var grade_name = accreditation.get_grade_name(current_grade)
+		
+		
+		var major_name = accreditation.get_major_name(current_major)
+		majors[i].set_text(major_name)
+		buttons[i].set_text(grade_name)
+		
+		buttons[i].set_button_icon(logos[major_index])
+		
+		majors[i].set_visible(true)
+		buttons[i].set_visible(true)
+		
+		var current_color = int( major_name.right(1) ) - 1
+		majors[i].set_self_modulate(colors[current_color])
+		buttons[i].set_self_modulate(colors[current_color])
+
+
+func set_refresh(p_refresh : bool = true) -> void:
+	refresh = false
+	if accreditation:
+		set_accreditation()
