1a1518789b3746f2269a68cfbb019332681097f0
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-01-24 20:06:36 +0700

    + Completed accreditation poster in accreditation_poster_3d.tscn + Added repeat_audio.gd to handle audio repetition

--- assets/vr/scenes/museums/accreditation/accreditation_poster.gd
+++ assets/vr/scenes/museums/accreditation/accreditation_poster.gd
@@ -4,8 +4,8 @@ class_name AccreditationPoster
 
 @export var accreditation : Accreditation = null
 @export var refresh : bool = false : set = set_refresh
-@export var primary_color : Color = Color.DARK_BLUE # : set = set_primary_color
-@export var secondary_color : Color = Color.WEB_PURPLE # : set = set_secondary_color
+# @export var primary_color : Color = Color.DARK_BLUE # : set = set_primary_color
+# @export var secondary_color : Color = Color.WEB_PURPLE # : set = set_secondary_color
 
 
 func _ready():
@@ -13,16 +13,16 @@ func _ready():
 	set_refresh()
 
 
-func set_primary_color(p_color : Color) -> void:
-	primary_color = p_color
-	$TopBanner.get_texture().gradient.set_color(0, primary_color)
-	$ColorRect/Ribbon.color = primary_color
-
-
-func set_secondary_color(p_color : Color) -> void:
-	secondary_color = p_color
-	$TopBanner.get_texture().gradient.set_color(1, secondary_color)
-	$BottomBanner.modulate = secondary_color
+#func set_primary_color(p_color : Color) -> void:
+	#primary_color = p_color
+	#$TopBanner.get_texture().gradient.set_color(0, primary_color)
+	#$ColorRect/Ribbon.color = primary_color
+#
+#
+#func set_secondary_color(p_color : Color) -> void:
+	#secondary_color = p_color
+	#$TopBanner.get_texture().gradient.set_color(1, secondary_color)
+	#$BottomBanner.modulate = secondary_color
 
 
 func set_refresh(_p_refresh : bool = true) -> void:
@@ -33,8 +33,8 @@ func set_refresh(_p_refresh : bool = true) -> void:
 		%Year.text = str(accreditation.year)
 		%Certificate.texture = accreditation.certificate
 		
-		set_primary_color(primary_color)
-		set_secondary_color(secondary_color)
+		#set_primary_color(primary_color)
+		#set_secondary_color(secondary_color)
 
 
 func _on_button_pressed():
